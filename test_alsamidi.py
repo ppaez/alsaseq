88803abfb93e9ea907bebe949cf7881642700c82
Patricio Paez <pp@pp.com.mx>
2014-01-19 19:25:49 -0600

    Added unit tests

--- test_alsamidi.py
+++ test_alsamidi.py
@@ -1,5 +1,56 @@
 from unittest import TestCase
 
+
+class NoteEvent(TestCase):
+
+    def test(self):
+        from alsamidi import noteevent
+
+        midi_event = (1, 60, 127, 1000, 10)
+        data = (1, 60, 127, 0, 10)
+        expected = (5, 1, 0, 0, (1, 0), (0, 0), (0, 0), data)
+        self.assertEqual(expected, noteevent(*midi_event))
+
+
+class NoteOnEvent(TestCase):
+
+    def test(self):
+        from alsamidi import noteonevent
+
+        midi_event = (1, 60, 127)
+        data = (1, 60, 127, 0, 0)
+        expected = (6, 1, 0, 253, (0, 0), (0, 0), (0, 0), data)
+        self.assertEqual(expected, noteonevent(*midi_event))
+
+
+class NoteOffEvent(TestCase):
+
+    def test(self):
+        from alsamidi import noteoffevent
+
+        midi_event = (1, 60, 10)
+        data = (1, 60, 10, 0, 0)
+        expected = (7, 1, 0, 253, (0, 0), (0, 0), (0, 0), data)
+        self.assertEqual(expected, noteoffevent(*midi_event))
+
+
+class PgmChangeEvent(TestCase):
+
+    def test_sent_directly(self):
+        from alsamidi import pgmchangeevent
+
+        data = (1, 0, 0, 0, 0, 9)
+        expected = (11, 1, 0, 253, (0, 0), (0, 0), (0, 0), data)
+        self.assertEqual(expected, pgmchangeevent(1, 9))
+
+    def test_scheduled(self):
+        from alsamidi import pgmchangeevent
+
+        data = (1, 0, 0, 0, 0, 9)
+        expected = (11, 1, 0, 0, (1, 0), (0, 0), (0, 0), data)
+        self.assertEqual(expected, pgmchangeevent(1, 9, 1000))
+
+
 class Merge(TestCase):
 
     def test_no_tracks(self):
