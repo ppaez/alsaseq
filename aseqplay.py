9941a0cacc72a86ecc2b0d76353c526fe7a6525a
Patricio Paez <pp@pp.com.mx>
2014-02-04 07:21:04 -0600

    Allow to be imported
    - Added main()

--- aseqplay.py
+++ aseqplay.py
@@ -3,31 +3,35 @@
 
 import alsaseq, alsamidi, sys
 
-dest_cliente = int( sys.argv[1] )
-ruta = sys.argv[2]
-if len(sys.argv) > 3:
-    display = sys.argv[3]
-else:
-    display = ''
+def main():
+    dest_cliente = int( sys.argv[1] )
+    ruta = sys.argv[2]
+    if len(sys.argv) > 3:
+        display = sys.argv[3]
+    else:
+        display = ''
 
-seq = alsamidi.Seq()
-seq.read( ruta )
-eventos = alsamidi.merge( seq.tracks )
-seq.info()
+    seq = alsamidi.Seq()
+    seq.read( ruta )
+    eventos = alsamidi.merge( seq.tracks )
+    seq.info()
 
-print len( eventos ), 'eventos'
-alsaseq.client( 'Reproductor', 0, 1, 1 )
-alsaseq.connectto( 0, dest_cliente, 0 )
+    print len( eventos ), 'eventos'
+    alsaseq.client( 'Reproductor', 0, 1, 1 )
+    alsaseq.connectto( 0, dest_cliente, 0 )
 
-for channel in range( 16 ):
-    alsaseq.output( alsamidi.pgmchangeevent( channel, 0 ) )
+    for channel in range( 16 ):
+        alsaseq.output( alsamidi.pgmchangeevent( channel, 0 ) )
 
-alsaseq.start()
+    alsaseq.start()
 
-#eventos = alsamidi.modifyevents( eventos, source = ( 20, 0 ) )
-for evento in eventos:
-    if display: print evento
-    alsaseq.output( evento )
+    #eventos = alsamidi.modifyevents( eventos, source = ( 20, 0 ) )
+    for evento in eventos:
+        if display: print evento
+        alsaseq.output( evento )
 
-alsaseq.syncoutput()
-# time.sleep( delay )
+    alsaseq.syncoutput()
+    # time.sleep( delay )
+
+if __name__ == '__main__':
+    main()
